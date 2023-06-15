1e2b3f90feeec7ffd7b134f4909d9b746bf90db6
User <adityataitkar@gmail.com>
2023-06-15 15:03:55 +0530

    Added Discovery server and Api Gateway

--- API-Gateway/src/main/java/com/Aditya/APIGateway/ApiGatewayApplication.java
+++ API-Gateway/src/main/java/com/Aditya/APIGateway/ApiGatewayApplication.java
@@ -1,15 +1,16 @@
-package com.Aditya.OrderManagement;
+package com.Aditya.APIGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+
 @EnableDiscoveryClient
-public class OrderManagementApplication {
+@SpringBootApplication
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderManagementApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
--- DrugsManagement/src/main/java/com/Aditya/DrugsManagement/DrugsManagementApplication.java
+++ DrugsManagement/src/main/java/com/Aditya/DrugsManagement/DrugsManagementApplication.java
@@ -2,8 +2,10 @@ package com.Aditya.DrugsManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DrugsManagementApplication {
 
 	public static void main(String[] args) {
